95deb020a25bbcb8c3af6711016cb07f323803a5
Mikel Mendoza <mikelmendozarubio89@gmail.com>
2018-10-29 23:38:49 +0100

    Logico funciona

--- src/basicosmikelmendozarubio/BasicosMikelMendozaRubio.java
+++ src/basicosmikelmendozarubio/BasicosMikelMendozaRubio.java
@@ -1,10 +1,20 @@
-
 package basicosmikelmendozarubio;
 
 public class BasicosMikelMendozaRubio {
 
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+      public static void main(String args[]){
+  
+   int matematicas = 9;
+   int biologia    = 7;
+   int quimica     = 7;
+   int promedio    = 0;
+
+   promedio = (matematicas + biologia + quimica) / 3;
+
+   if(promedio >= 6){
+     System.out.println("El alumno aprobó " + promedio);
+   } else {
+     System.out.println("con este tipo de variable no se acepta ");
+   }
+ }
 }
