46c7b926262a7b7a263757b139115bf85e2f2361
Mikel Mendoza <mikelmendozarubio89@gmail.com>
2018-10-29 23:06:17 +0100

    master

---
+++ src/basicosmikelmendozarubio/BasicosMikelMendozaRubio.java
@@ -0,0 +1,10 @@
+
+package basicosmikelmendozarubio;
+
+public class BasicosMikelMendozaRubio {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
