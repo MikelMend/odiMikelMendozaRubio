e62ed90d3c935c53bbb304ca8edf1451c9870f8f
Mikel Mendoza <mikelmendozarubio89@gmail.com>
2018-10-29 23:25:33 +0100

    cadenas

--- src/basicosmikelmendozarubio/BasicosMikelMendozaRubio.java
+++ src/basicosmikelmendozarubio/BasicosMikelMendozaRubio.java
@@ -1,10 +1,23 @@
-
-package basicosmikelmendozarubio;
+package BasicosMikelMendozaRubio;
 
 public class BasicosMikelMendozaRubio {
 
+    /**
+     * Método para mostrar ejemplos de tipos numericos.
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        String cadena = "El MAR ES AZUL Y EL CIELO TAMBIEN";
+
+        int longitud = cadena.length();
+
+        System.out.println("Ejemplo 1" + cadena.substring(5));
+        System.out.println("Ejemplo 1" + cadena.substring(9));
+        System.out.println("Ejemplo 1" + cadena.substring(12));
+        System.out.println("Ejemplo 1" + cadena.substring(1, 10));
+        System.out.println("Ejemplo 1" + cadena.substring(5, 20));
+
     }
-    
 }
